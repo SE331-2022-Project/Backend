76779336afd01ea274566a17f094cbe12da5b59c
kittisak <kittisak_kae@cmu.ac.th>
2022-10-30 16:48:23 +0700

    post method of patient

--- src/main/java/se331/rest/service/PatientServiceImpl.java
+++ src/main/java/se331/rest/service/PatientServiceImpl.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import se331.rest.dao.DoctorDao;
 import se331.rest.dao.PatientDao;
+import se331.rest.entity.Doctor;
 import se331.rest.entity.Patient;
 
 import javax.transaction.Transactional;
@@ -13,6 +15,8 @@ import javax.transaction.Transactional;
 public class PatientServiceImpl implements PatientService {
     @Autowired
     PatientDao patientDao;
+    @Autowired
+    DoctorDao doctorDao;
     @Override
     public Integer getPatientSize() {
         return patientDao.getPatientSize();
@@ -31,6 +35,9 @@ public class PatientServiceImpl implements PatientService {
     @Override
     @Transactional
     public Patient save(Patient patient) {
+        Doctor doctor = doctorDao.findById(patient.getDoctor().getId()).orElse(null);
+        patient.setDoctor(doctor);
+        doctor.getPatients().add(patient);
         return patientDao.save(patient);
     }
 
