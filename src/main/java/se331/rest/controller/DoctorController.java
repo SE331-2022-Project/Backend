041f6a1c9a0bc83bded145033eca0196a053151c
konnRG <108419967+konnRG@users.noreply.github.com>
2022-10-31 22:12:06 +0700

    Fix Doctor's Patient List.

--- src/main/java/se331/rest/controller/DoctorController.java
+++ src/main/java/se331/rest/controller/DoctorController.java
@@ -36,15 +36,9 @@ public class DoctorController {
         }
     }
     @GetMapping("/doctors/{id}/patients")
-    public ResponseEntity<?> getPatientList(@RequestParam(value = "_limit", required = false) Integer perPage
-            , @RequestParam(value = "_page", required = false) Integer page
-            , @PathVariable("id") Long id) {
-        perPage = perPage == null ? 3 : perPage;
-        page = page == null ? 1 : page;
-        Page<Doctor> pageOutput = null;
-        pageOutput = doctorService.getDoctor(perPage,page);
-        HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
-        return new ResponseEntity<>(LabMapper.INSTANCE.getDoctorDTO(pageOutput.getContent()), responseHeader, HttpStatus.OK);
+    ResponseEntity getDoctorPatient(@PathVariable("id") Long id) {
+        Doctor doctor = doctorService.getDoctor(id);
+        List<Patient> patientList = doctor.getPatients();
+        return ResponseEntity.ok(LabMapper.INSTANCE.getPatientDTO(patientList));
     }
 }
